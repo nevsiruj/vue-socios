b61592bd77fdcd5c28bf0d17bda028a5d4cde684
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-26 00:47:21 -0300

    1 file modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -45,7 +45,7 @@
               ></path>
             </svg>
           </button>
-          <a href="https://flowbite.com" class="flex ml-2 md:mr-24">
+          <a href="/" class="flex ml-2 md:mr-24">
             <img
               src="https://flowbite.com/docs/images/logo.svg"
               class="h-8 mr-3"
@@ -60,7 +60,7 @@
                 whitespace-nowrap
                 dark:text-white
               "
-              >Flowbite</span
+              >Cooperadora</span
             >
           </a>
         </div>
