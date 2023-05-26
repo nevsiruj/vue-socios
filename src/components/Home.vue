45873abd63f2e3ddb7c0a2035d9f851ec039c3f8
nevsiruj <98376961+nevsiruj@users.noreply.github.com>
2023-05-26 01:17:23 -0300

    1 file modified

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,35 @@
 <template>
-  <p>home works</p>
+  <div class="mt-8">
+    <router-link
+      to="/list-socio"
+      class="
+        bg-blue-500
+        text-white
+        px-4
+        py-2
+        rounded-md
+        hover:bg-blue-600
+        focus:outline-none focus:bg-blue-600
+      "
+    >
+      Ir a socios
+    </router-link>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
   components: {},
+  methods: {
+    goToSocios() {
+      // Aquí puedes añadir la lógica para redirigir a la página de socios
+      // Por ejemplo, utilizando Vue Router: this.$router.push('/socios')
+    },
+  },
 };
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+/* Estilos adicionales si los necesitas */
 </style>
